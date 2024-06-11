505973457ac1f02f217f9805f1a2fb4404ffa264
Paul Marunko <marunkopavlo@gmail.com>
2024-06-10 22:40:45 -0600

    f1

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/calculation', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'Calculation.html'));
+});
+
+app.get('/about', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
